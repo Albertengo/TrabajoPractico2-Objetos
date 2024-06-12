d6f52dcbf8fc431a28f765fac89a7c66a43a5ff8
Micaela-Boler <bolermica@gmail.com>
2024-06-11 22:55:14 -0300

    Cambios en script de enemigo
    Patrullaje incompleto y nuevas líneas de código en el método perseguir

--- Assets/Scripts/Enemigos/Enemy.cs
+++ Assets/Scripts/Enemigos/Enemy.cs
@@ -2,10 +2,34 @@ using Jugador;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MovController
 {
     protected JugadorMov Jugador;
+
+    [SerializeField] protected Transform objetivo;
+    [SerializeField] protected NavMeshAgent agente;
+
+    [Header("Patrullaje")]
+    [SerializeField] protected Transform[] puntosDeMovimiento;
+    [SerializeField] protected float distaciaMinima;
+    protected int numeroAleatorio;
+
+
+    private void Awake()
+    {
+        agente = GetComponent<NavMeshAgent>();
+
+        agente.updateRotation = false;
+        agente.updateUpAxis = false;
+
+        objetivo = GameObject.FindGameObjectWithTag("Jugador").transform;
+
+        // PANTRULLAJE //
+        numeroAleatorio = Random.Range(0, puntosDeMovimiento.Length);
+    }
 
 
     protected override void Atacar(int daño)
@@ -21,12 +45,20 @@ public class Enemy : MovController
     protected override void Movimiento(Transform pos)
     {
         //Patrullaje
+
+        //Se mueve hacia un punto
+        transform.position = Vector2.MoveTowards(transform.position, puntosDeMovimiento[numeroAleatorio].position, velocidad * Time.deltaTime);
+
+        //Si llega al punto anterior, busca una nuevo
+        if (Vector2.Distance(transform.position, puntosDeMovimiento[numeroAleatorio].position) < distaciaMinima)
+            numeroAleatorio = Random.Range(0, puntosDeMovimiento.Length);
     }
 
 
     protected virtual void Perseguir()
     {
-        // Lógica para seguir al jugador
+        if (objetivo != null)
+            agente.SetDestination(objetivo.position);
     }
 
 
