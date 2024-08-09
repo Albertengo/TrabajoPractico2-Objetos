55a30001d21702fcf2417ab02cd95a81b6e81341
Micaela-Boler <bolermica@gmail.com>
2024-08-08 21:20:25 -0300

    Pequeños arreglos y script de vida

--- Assets/Scripts/Player/VidaJugador.cs
+++ Assets/Scripts/Player/VidaJugador.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class VidaJugador : VidaYAtaque, IRecibirDaño
 {
     public GameManager gameManager;
+    private bool puedeRecibirDaño = true;
+
 
     public void TomarDaño(int daño)
     {
         if (Vida > 0)
         {
-            Vida -= daño;
-            gameManager.CambiarCorazon(Vida);
-
-
-            //Animacion de recibir daño
+           if (puedeRecibirDaño) 
+           {
+                Vida -= daño;
+                StartCoroutine(Inmunidad());
+                gameManager.CambiarCorazon(Vida);
+           }
         }
         else
         {
             //gameManager.CambiarEscena(3);
             Debug.Log("MORISTE!!!!!!!!!!!!!!!"); // ACTIVAR PANTALLA DE DERROTA
         }
+    }
+
+    IEnumerator Inmunidad()
+    {
+        puedeRecibirDaño = false;
+
+        yield return new WaitForSeconds(2);
+
+        puedeRecibirDaño = true;
     }
 }
